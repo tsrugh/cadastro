afe121820b5eda0b04329ffda9b91c79d472cae8
tsrugh <lucas_3600@hotmail.com.br>
2024-04-02 23:44:03 -0300

    [Include] First Route

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import conn from "./src/connection/connection.js";
 import {promisify} from 'util'
+import http from 'http'
 
 function main(){
     
@@ -20,4 +21,36 @@ function main(){
 }
 
 
-main();
+const routes = {
+
+    '/listar:get': (request, response) => {
+
+        response.write('Bom dia')
+        return response.end()
+
+    },
+    default(request, response) {
+        response.writeHead(404)
+        return response.end('Not found')
+    } 
+
+
+
+}
+
+
+const handler = (request, response) => {
+
+    const {url, method} = request;
+    const routeKey = `${url.toLowerCase()}:${method.toLowerCase()}`;
+    const chosen = routes[routeKey] || routes.default
+    return chosen(request, response)
+
+} 
+
+const app = http.createServer(handler)
+.listen(3000, () => console.log('Running at 3000'))
+
+
+//main();
+
